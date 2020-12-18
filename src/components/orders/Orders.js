ec03bc2e3eb13c6f479f17b751a120356ebcb0f6
qwertyEight <8qwerty7654321@gmail.com>
2020-12-18 13:36:03 +0300

    first commit

---
+++ src/components/orders/Orders.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+
+const Orders = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Orders Page</h1>
+    </div>
+  );
+};
+
+export default Orders;
---
+++ src/router/Router.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "../components/dashboard/Dashboard";
+import Orders from "../components/orders/Orders";
+import Login from "../components/login/Login";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/orders" component={Orders} />
+      </Switch>
+    </Router>
+  );
+};
+
+// const DefaultContainer = () => (
+//   <div>
+//     <Navbar />
+//     <Route path="/dashboard" component={Dashboard} />
+//     <Route path="/orders" component={Orders} />
+//   </div>
+// );
+
+export default AppRouter;
+
+// Zz.12345678Github
