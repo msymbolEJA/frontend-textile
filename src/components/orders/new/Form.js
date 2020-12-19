725911f4bdf448e1d5a14084366e06958afbeb41
qwertyEight <8qwerty7654321@gmail.com>
2020-12-19 16:22:21 +0300

    add ui form at new-order page

--- src/components/orders/new/Form.js
+++ src/components/orders/new/Form.js
@@ -1,41 +1,143 @@
-const InputForm = ({ handleSubmit, handleChange }) => {
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>Desert:</label>
-        <br />
-        <input type="text" name="desert" id="desert" onChange={handleChange} />
-        <br />
-        <label>Calories(g):</label>
-        <br />
-        <input
-          type="number"
-          name="calories"
-          id="calories"
-          onChange={handleChange}
-        />
-        <br />
-        <label>Fat:</label>
-        <br />
-        <input type="number" name="fat" id="fat" onChange={handleChange} />
-        <br />
-        <label>Carbs:</label>
-        <br />
-        <input type="number" name="carbs" id="carbs" onChange={handleChange} />
-        <br />
-        <label>Protein:</label>
-        <br />
-        <input
-          type="number"
-          name="protein"
-          id="protein"
-          onChange={handleChange}
-        />
-        <br />
-        <input type="submit" value="Add Order" />
-      </form>
-    </div>
-  );
-};
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import LocalMallIcon from "@material-ui/icons/LocalMall";
 
-export default InputForm;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    height: "auto",
+    marginTop: "0.8rem",
+  },
+  image: {
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1548142723-aae7678afa53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(4, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function InputForm({ handleSubmit, handleChange }) {
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            New Order
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="desert"
+              label="Desert"
+              name="desert"
+              autoComplete="desert"
+              onChange={handleChange}
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="calories"
+              label="Calories"
+              type="number"
+              id="calories"
+              autoComplete="calories"
+              onChange={handleChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="fat"
+              label="Fat"
+              type="number"
+              id="fat"
+              autoComplete="fat"
+              onChange={handleChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="carbs"
+              label="Carbs"
+              type="number"
+              id="calories"
+              autoComplete="calories"
+              onChange={handleChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="protein"
+              label="Protein"
+              type="number"
+              id="protein"
+              autoComplete="protein"
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Create
+            </Button>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
