cbcb1d375eb0f968b39e2448e683cdbe1349b8dc
mssymbolE <mssymbol@protonmail.com>
2020-12-27 00:21:53 +0300

    add due-date but not completed

---
+++ src/helper/Functions.js
@@ -0,0 +1,20 @@
+const getHeaders = (data) => {
+    let headerArr = [];
+    let i = 0;
+    for (i = 0; i < data.length; i++) {
+      headerArr[i] = data[i].date;
+    }
+    return headerArr;
+  };
+  
+  const getBodyItems = (data) => {
+    let bodytemsArr = [];
+    let i = 0;
+    for (i = 0; i < data.length; i++) {
+      bodytemsArr = [...bodytemsArr, data[i].orders];
+    }
+    return bodytemsArr;
+  };
+  
+  export { getHeaders, getBodyItems };
+  
