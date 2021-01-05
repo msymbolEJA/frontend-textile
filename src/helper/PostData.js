a311a0d42318d3ecd733aaeee3299b614522812a
mssymbolE <mssymbol@protonmail.com>
2021-01-05 12:11:32 +0300

    solved register problem

--- src/helper/PostData.js
+++ src/helper/PostData.js
@@ -1,12 +1,10 @@
 import axios from "axios"
 
 export const postData = async (path, data) => {
-    const token = "token";
     const response = await axios.post(`${path}`, data, {
         headers: {
             Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + token // if you use token
+            'Content-Type': 'application/json'
         }
     })
     return response?.data
