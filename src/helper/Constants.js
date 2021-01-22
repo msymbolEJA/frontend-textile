36f3635342890514c4b3c3696738e782fd0e1ac3
ed ben <edwardbenedict92@gmail.com>
2021-01-22 22:08:20 +0300

    add constant

--- src/helper/Constants.js
+++ src/helper/Constants.js
@@ -1,7 +1,19 @@
 export const tagsData = [
     "all orders",
     "awaiting",
-    "processing",
+    "in-progress",
+    "ready",
+    "in_transit",
+    "cancelled",
+    "shipped",
+    "repeat",
+    "follow_up",
+    "pending"
+];
+
+export const statusData = [
+    "awaiting",
+    "in-progress",
     "ready",
     "in_transit",
     "cancelled",
