efccf3f81832e5afcdf006935633c3a6da63fa3e
ed ben <edwardbenedict92@gmail.com>
2021-01-23 16:02:10 +0300

    set awaiting orders to approval pn dashboard

--- src/components/dashboard/AwaitingOrders.js
+++ src/components/dashboard/AwaitingOrders.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import HourglassEmptyIcon from "@material-ui/icons/HourglassEmpty";
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -25,14 +25,14 @@ export default function AwaitingOrders() {
   const history = useHistory();
 
   const handleClick = () => {
-    history.push("/order-prep");
+    history.push("/approval");
   };
 
   return (
     <Grid item xs={12} md={6} onClick={handleClick} className={classes.root}>
       <Paper className={classes.paper}>
-        <HourglassEmptyIcon className={classes.icon} color="primary" />
-        <h1>Awaiting Orders</h1>
+        <ThumbUpIcon className={classes.icon} color="primary" />
+        <h1>Approval Orders</h1>
       </Paper>
     </Grid>
   );
