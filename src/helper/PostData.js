ac068b5e6f93a437ae539596f1b089369f7082b0
ed ben <edwardbenedict92@gmail.com>
2021-01-26 18:32:41 +0300

    edit getAllPdf func feom post to get on PostData

--- src/helper/PostData.js
+++ src/helper/PostData.js
@@ -1,70 +1,70 @@
-import axios from "axios"
-import  FormData from 'form-data';
+import axios from "axios";
+import FormData from "form-data";
 
 export const postData = async (path, data) => {
-    const response = await axios.post(`${path}`, data, {
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        }
-    })
-    return response
-}
+  const response = await axios.post(`${path}`, data, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  return response;
+};
 
-export const putData = async(path,data) => {
-    const response = await axios.put(`${path}`, data, {
-        headers:{
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        }
-    })
-    return response
-}
+export const putData = async (path, data) => {
+  const response = await axios.put(`${path}`, data, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  return response;
+};
 
-export const getData = async(path,data) => {
-    const response = await axios.get(`${path}`, data, {
-        headers:{
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        }
-    })
-    return response
-}
+export const getData = async (path, data) => {
+  const response = await axios.get(`${path}`, data, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  return response;
+};
 
-export  const postFormData = async (path, dataObj) =>  {
-    const data = new FormData();
-    console.log("data1",data)
-    Object.keys(dataObj).forEach(key => data.append(key, dataObj[key]));
+export const postFormData = async (path, dataObj) => {
+  const data = new FormData();
+  console.log("data1", data);
+  Object.keys(dataObj).forEach((key) => data.append(key, dataObj[key]));
 
-    console.log("data",data)
-    const response = await axios.post(`${path}`, data, {
-      headers:{
-        'Content-Type': 'multipart/form-data'
-      }
-  })
-  return response
-}
+  console.log("data", data);
+  const response = await axios.post(`${path}`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response;
+};
 
 export const queryData = async (path) => {
-    const response = await axios.get(`${path}`)
-    return response
-}
+  const response = await axios.get(`${path}`);
+  return response;
+};
 
 export const putImage = async (path, image, imageName) => {
-    const fd = new FormData();
-    fd.append("Image", image, imageName)
-    const response = await axios.put(path, fd)
-    return response
-}
+  const fd = new FormData();
+  fd.append("Image", image, imageName);
+  const response = await axios.put(path, fd);
+  return response;
+};
 
 export const getOnePdf = async (path, data) => {
-    const fd = new FormData();
-    fd.append("a", 2)
-    const response = await axios.post(path, fd)
-    return response
-}
+  const fd = new FormData();
+  fd.append("a", 2);
+  const response = await axios.post(path, fd);
+  return response;
+};
 
-export const getAllPdf = async (path,data)=>{
-    const response = await axios.post(path)
-    return response
-}
+export const getAllPdf = async (path, data) => {
+  const response = await axios.get(path);
+  return response;
+};
