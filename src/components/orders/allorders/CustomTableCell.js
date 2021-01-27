2774fd66871b7e41564ec98ae0598c5950820158
ed ben <edwardbenedict92@gmail.com>
2021-01-27 18:58:17 +0300

    remove unnecessary codes on CustomTableCell on Allorders table

--- src/components/orders/allorders/CustomTableCell.js
+++ src/components/orders/allorders/CustomTableCell.js
@@ -1,35 +1,24 @@
 import TableCell from "@material-ui/core/TableCell";
 import { makeStyles } from "@material-ui/core/styles";
-import Input from "@material-ui/core/Input";
 
 const useStyles = makeStyles((theme) => ({
-    tableCell: {
-        width: 130,
-        height: 40,
-    },
-    input: {
-        width: 130,
-        height: 40,
-    },
+  tableCell: {
+    width: 130,
+    height: 40,
+  },
+  input: {
+    width: 130,
+    height: 40,
+  },
 }));
 
-const CustomTableCell = ({ row, name, onChange }) => {
-    const classes = useStyles();
-    const { isEditMode } = row;
-    return (
-        <TableCell align="center" className={classes.tableCell}>
-            {isEditMode ? (
-                <Input
-                    value={row[name]}
-                    name={name}
-                    onChange={(e) => onChange(e, row)}
-                    className={classes.input}
-                />
-            ) : (
-                    row[name]
-                )}
-        </TableCell>
-    );
+const CustomTableCell = ({ row, name }) => {
+  const classes = useStyles();
+  return (
+    <TableCell align="center" className={classes.tableCell}>
+      {row[name]}
+    </TableCell>
+  );
 };
 
 export default CustomTableCell;
