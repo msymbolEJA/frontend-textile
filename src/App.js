cfbe4be193f45828e149f00d27ac8c0800a6a6f2
ms <mustafa.sahin.co@gmail.com>
2021-01-31 15:24:39 +0200

    Update toaster

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import AppRouter from "./router/Router";
 import { ContextProvider } from "./context/Context";
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <ContextProvider>
         <AppRouter />
+        <ToastContainer />
       </ContextProvider>
     </div>
   );
