dbc04758333259e0ce106e5c17aa4e0f4460cc9f
ed ben <edwardbenedict92@gmail.com>
2021-02-08 18:47:33 +0300

    add userRole to context

--- src/context/Context.js
+++ src/context/Context.js
@@ -10,10 +10,20 @@ export const ContextProvider = (props) => {
   // console.log("localUser", localUser);
   // console.log("user.role", user.role);
 
-  const isAdmin = (user.role || localUser) === "admin";
-  //console.log(isAdmin);
+  const userRole = user.role || localUser;
+
+  const isAdmin =
+    userRole === "admin" ||
+    userRole === "shop_manager" ||
+    userRole === "shop_packer";
+  // console.log("isAdmin", isAdmin);
+
+  //console.log("userRole", userRole);
+
   return (
-    <AppContext.Provider value={{ user, setUser, auth, setAuth, isAdmin }}>
+    <AppContext.Provider
+      value={{ user, setUser, auth, setAuth, isAdmin, userRole }}
+    >
       {props.children}
     </AppContext.Provider>
   );
