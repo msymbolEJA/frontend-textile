839649f908dbcb7f0d6a161f6f60dc65ba7f60be
ed ben <edwardbenedict92@gmail.com>
2021-02-18 14:15:46 +0300

    v.2.0 Belky release

--- src/helper/Constants.js
+++ src/helper/Constants.js
@@ -69,4 +69,5 @@ export const repeatReasons = {
 export const STORE_BASE_URLS = {
   BELKY: "http://45.76.57.100:8080/",
   SHINY: "http://144.202.67.136:8080/",
+  SILVERISTIC: "http://45.32.195.132:8080/",
 };
