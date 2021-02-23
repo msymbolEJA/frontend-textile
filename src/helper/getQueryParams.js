29c3195aa52aba89616d2f4e04b7d2f3253cbb02
ed ben <edwardbenedict92@gmail.com>
2021-02-23 17:23:29 +0300

    add comment to getQueryParams2

--- src/helper/getQueryParams.js
+++ src/helper/getQueryParams.js
@@ -9,11 +9,11 @@ export const getQueryParams = (t = window.location.search) => {
     );
 };
 
-const getQueryParams2 = () =>
-  window.location.search
-    ?.replace("?", "")
-    .split("&")
-    .reduce(
-      (r, e) => ((r[e.split("=")[0]] = decodeURIComponent(e.split("=")[1])), r),
-      {}
-    );
+// const getQueryParams2 = () =>
+//   window.location.search
+//     ?.replace("?", "")
+//     .split("&")
+//     .reduce(
+//       (r, e) => ((r[e.split("=")[0]] = decodeURIComponent(e.split("=")[1])), r),
+//       {}
+//     );
