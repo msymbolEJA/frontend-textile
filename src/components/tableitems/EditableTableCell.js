01fd517623936642932accc3fd62e43dc5a80516
ms <mustafa.sahin.co@gmail.com>
2021-03-08 17:26:13 +0200

    Change editable fields to textarea

--- src/components/tableitems/EditableTableCell.js
+++ src/components/tableitems/EditableTableCell.js
@@ -1,26 +1,33 @@
 import { makeStyles } from "@material-ui/core/styles";
 import TableCell from "@material-ui/core/TableCell";
-import Input from "@material-ui/core/Input";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 
 const useStyles = makeStyles(() => ({
   tableCell: {
     padding: 0,
-    width: "fit-content",
+    minWidth: "100px",
+    maxWidth: "100px",
     borderRight: "0.5px solid grey",
   },
   input: {
     fontSize: "0.75rem",
+    maxHeight: "100%",
+    width: "100%",
+    minWidth: "90px",
+    maxWidth: "90px",
     //wordWrap: "break-word"
   },
 }));
-
+/* <TextareaAutosize aria-label="empty textarea" placeholder="Empty" />
+ */
 const EditableTableCell = ({ row, name, onChange }) => {
   const classes = useStyles();
   const { isEditMode } = row;
   return (
     <TableCell align="center" className={classes.tableCell}>
       {isEditMode ? (
-        <Input
+        <TextareaAutosize
+          rowsMin={3}
           value={row[name] ? row[name] : ""} // first : value={row[name]} // i've changed
           name={name}
           onChange={(e) => onChange(e, row)}
