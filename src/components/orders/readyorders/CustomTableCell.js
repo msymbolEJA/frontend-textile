3533f1d3cee64af51a2bb0edc19f1d23c5650249
ms <mustafa.sahin.co@gmail.com>
2021-04-18 17:38:04 +0300

    Add replace to order detail explanation

--- src/components/orders/readyorders/CustomTableCell.js
+++ src/components/orders/readyorders/CustomTableCell.js
@@ -44,6 +44,15 @@ const CustomTableCell = ({ row, name, name2, onChange }) => {
               ? "APPROVED"
               : row[name].replace("_", " "),
         })
+      ) : name === "explanation" ? (
+        row[name]
+          .replace("_", " ")
+          .replace("REPEAT", "TEKRAR")
+          .replace("MANUFACTURING ERROR", "ÜRETİM HATASI")
+          .replace("DISCOLORATION", "RENK ATMA")
+          .replace(": BREAK OFF", "")
+          .replace(": LOST IN MAIL", "")
+          .replace(": SECOND", "")
       ) : (
         row[name]
       )}
