8a0b99a25e8700a5e632bc6a126206c4f93ca478
edwardBenedict <edwardbenedict92@gmail.com>
2022-04-03 14:31:19 +0200

    fix: repeatReasonsLinen imported

--- src/components/approval/Approval.js
+++ src/components/approval/Approval.js
@@ -24,6 +24,7 @@ import {
   // ThumbUpAlt as ThumbUpAltIcon,
 } from "@material-ui/icons";
 import {
+  repeatReasonsLinen,
   repeatReasonsMenuItemsLinenia,
   statusData,
 } from "../../helper/Constants";
