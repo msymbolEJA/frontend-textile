0fb8d9e6208335bf99280b9a18d1fdc721971fdd
Emre Yoleri <emreyoleri01@hotmail.com>
2023-06-10 22:10:23 +0300

    Update best seller table

--- src/components/best-seller/SellerTable.js
+++ src/components/best-seller/SellerTable.js
@@ -1,12 +1,15 @@
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import { FormattedMessage } from "react-intl";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  makeStyles,
+} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   tContainer: {
@@ -15,8 +18,8 @@ const useStyles = makeStyles(theme => ({
     marginTop: 10,
   },
   rowWithNull: {
-    backgroundColor: 'black',
-    color: 'white',
+    backgroundColor: "black",
+    color: "white",
   },
   tableCellHeader: {
     color: "black",
@@ -25,37 +28,26 @@ const useStyles = makeStyles(theme => ({
     padding: 13,
   },
   darkTableRow: {
-    backgroundColor: '#F2F2F2',
+    backgroundColor: "#F2F2F2",
   },
   tableCell: {
-    fontFamily: 'Courier New',
-    border: '1px solid gray',
-    borderBottom: '2px solid black',
-    borderTop: '2px solid black',
+    fontFamily: "Courier New",
+    border: "1px solid gray",
+    borderBottom: "2px solid black",
+    borderTop: "2px solid black",
     padding: 13,
   },
   tablePaper: {
     marginTop: "10px",
     marginBottom: "10px",
-    // border: '1px solid lightgrey',
     borderRadius: "5px",
     overflowX: "auto",
   },
   bottom: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
   },
   table: {},
-  boldText: {
-    fontWeight: 'bold',
-  },
-  blackGround: {
-    backgroundColor: 'black',
-    color: 'white',
-  },
-  centerCell: {
-    textAlign: 'center',
-  },
   boldText: {
     fontWeight: "bold",
   },
@@ -70,23 +62,17 @@ const useStyles = makeStyles(theme => ({
 
 const SellerTable = ({ bestRows }) => {
   const classes = useStyles();
-  const sortByShop = items => {
-    return items.sort((a, b) => a.shop.localeCompare(b.shop));
-  };
 
   const groupByTypeAndColor = items => {
-    return items.reduce((result, item, index) => {
-      const { shop, type, color, total_cost, labor_cost, count, goldGr } = item;
+    return items.reduce((result, item) => {
+      const { type, color, total_cost, labor_cost, count, goldGr } = item;
       const key = `${type}`;
 
       if (!result[key]) {
         result[key] = {
           total_cost: 0,
-          labor_cost: 0,
           count: 0,
           colors: {},
-          goldGr: goldGr ? goldGr : null,
-          shop: shop,
         };
       }
 
@@ -97,111 +83,105 @@ const SellerTable = ({ bestRows }) => {
         result[key].colors[color] = {
           total_cost: 0,
           count: 0,
+          labor_cost: [],
+          goldGr: [],
         };
       }
 
       result[key].colors[color].total_cost += total_cost;
       result[key].colors[color].count += count;
-
-      if (labor_cost !== null) {
-        result[key].labor_cost += labor_cost;
-      }
+      result[key].colors[color].labor_cost.push(labor_cost);
+      result[key].colors[color].goldGr.push(goldGr);
 
       return result;
     }, {});
   };
 
   const renderTypeAndColorRows = groupedItems => {
-    return Object.keys(groupedItems).map((type, typeIndex) => {
-      const { total_cost, labor_cost, count, colors, goldGr, shop, nextShop } = groupedItems[type];
-
-      const colorRows = Object.keys(colors).map(color => {
-        const { total_cost: colorTotalCost, count: colorCount } = colors[color];
-
-        return (
-          <TableRow
-            key={`${type}-${color}`}
-            className={typeIndex % 2 === 1 ? classes.darkTableRow : null}
-          >
-            <TableCell align="center" className={classes.boldText}>
-              {color !== null ? `${color}` : "-"}
-            </TableCell>
-            <TableCell align="center" className={classes.boldText}>
-              {colorTotalCost !== null ? `$${colorTotalCost.toFixed(2)}` : "-"}
-            </TableCell>
-            <TableCell align="center" className={classes.boldText}>
-              {colorCount !== null ? `${colorCount}` : "-"}
-            </TableCell>
-            <TableCell align="center" className={classes.boldText}>
-              {labor_cost !== null ? `$${labor_cost.toFixed(2)}` : "-"}
-            </TableCell>
-            <TableCell align="center" className={classes.boldText}>
-              {goldGr !== null ? goldGr.toFixed(2) : "-"}
-            </TableCell>
-          </TableRow>
-        );
-      });
-
-      const typeRowSpan = Object.keys(colors).length;
-      let rowSpanned = false;
+    return Object.keys(groupedItems).map((type, i) => {
+      const { total_cost, count, colors } = groupedItems[type];
 
       return (
-        <>
-          {!rowSpanned && (
-            <>
-              <TableRow key={type} className={typeIndex % 2 === 1 ? classes.darkTableRow : null}>
-                <TableCell
-                  rowSpan={typeRowSpan + 1}
-                  className={`${classes.boldText} ${classes.centerCell}`}
-                  align="center"
-                >
-                  {type} <br />
-                  {` (Total Count: ${count}, Total Cost: $${total_cost.toFixed(2)})`}
+        <React.Fragment key={type}>
+          <TableRow className={i % 2 === 1 ? classes.darkTableRow : null}>
+            <TableCell
+              rowSpan={Object.keys(colors).length + 1}
+              className={`${classes.boldText} ${classes.centerCell}`}
+              align="center"
+            >
+              {type} <br />
+              {` (Total Count: ${count}, Total Cost: $${total_cost.toFixed(2)})`}
+            </TableCell>
+          </TableRow>
+          {Object.keys(colors).map(color => {
+            const {
+              total_cost: colorTotalCost,
+              count: colorCount,
+              labor_cost,
+              goldGr,
+            } = colors[color];
+
+            return (
+              <TableRow
+                key={`${type}-${color}`}
+                className={i % 2 === 1 ? classes.darkTableRow : null}
+              >
+                <TableCell align="center" className={classes.boldText}>
+                  {color !== null ? `${color}` : "-"}
+                </TableCell>
+                <TableCell align="center" className={classes.boldText}>
+                  {colorTotalCost !== null ? `$${colorTotalCost.toFixed(2)}` : "-"}
+                </TableCell>
+                <TableCell align="center" className={classes.boldText}>
+                  {colorCount !== null ? `${colorCount}` : "-"}
+                </TableCell>
+                <TableCell align="center" className={classes.boldText}>
+                  {labor_cost.length > 0
+                    ? `$${labor_cost.reduce((a, b) => a + b, 0).toFixed(2)}`
+                    : "-"}
+                </TableCell>
+                <TableCell align="center" className={classes.boldText}>
+                  {goldGr.length > 0 ? `$${goldGr.reduce((a, b) => a + b, 0).toFixed(2)}` : "-"}
                 </TableCell>
               </TableRow>
-            </>
-          )}
-          {colorRows}
-          {(rowSpanned = true)}
-        </>
+            );
+          })}
+        </React.Fragment>
       );
     });
   };
 
-  const sortedItems = sortByShop(bestRows);
+  const sortedItems = bestRows.sort((a, b) => a.shop.localeCompare(b.shop));
   const groupedByTypeAndColor = groupByTypeAndColor(sortedItems);
 
-  console.log(groupedByTypeAndColor);
   return (
     <div className={classes.bottom}>
       <div className={classes.tablePaper}>
-        <TableContainer className={classes.tContainer}>
+        <TableContainer className={classes.tContainer} component={Paper}>
           <Table className={classes.table}>
             <TableHead>
               <TableRow className={classes.tableCellHeader}>
                 <TableCell className={classes.boldText} align="center">
-                  <FormattedMessage id={"Type"} defaultMessage={"Type"} />
+                  <FormattedMessage id="Type" defaultMessage="Type" />
                 </TableCell>
                 <TableCell className={classes.boldText} align="center">
-                  <FormattedMessage id={"Color"} defaultMessage={"Color"} />
+                  <FormattedMessage id="Color" defaultMessage="Color" />
                 </TableCell>
                 <TableCell className={classes.boldText} align="center">
-                  <FormattedMessage id={"Total Cost"} defaultMessage={"Total Cost"} />
+                  <FormattedMessage id="Total Cost" defaultMessage="Total Cost" />
                 </TableCell>
                 <TableCell className={classes.boldText} align="center">
-                  <FormattedMessage id={"Total Count"} defaultMessage={"Total Count"} />
+                  <FormattedMessage id="Total Count" defaultMessage="Total Count" />
                 </TableCell>
                 <TableCell className={classes.boldText} align="center">
-                  <FormattedMessage id={"Labor Cost"} defaultMessage={"Labor Cost"} />
+                  <FormattedMessage id="Labor Cost" defaultMessage="Labor Cost" />
                 </TableCell>
                 <TableCell className={classes.boldText} align="center">
-                  <FormattedMessage id={"GoldGr"} defaultMessage={"GoldGr"} />
+                  <FormattedMessage id="GoldGr" defaultMessage="GoldGr" />
                 </TableCell>
               </TableRow>
             </TableHead>
-            {Object.values(groupedByTypeAndColor)?.[0] && (
-              <TableBody>{renderTypeAndColorRows(groupedByTypeAndColor)}</TableBody>
-            )}
+            <TableBody>{renderTypeAndColorRows(groupedByTypeAndColor)}</TableBody>
           </Table>
         </TableContainer>
       </div>
